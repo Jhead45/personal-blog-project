7f744456c9dbb1e9964fc4a2742da898dd0674d2
Justin Head <justin_head@ymail.com>
2018-05-07 15:06:13 -0500

    back end Authentication added

--- server/src/index.js
+++ server/src/index.js
@@ -4,6 +4,8 @@ import express from 'express';
 import morgan from 'morgan';
 import routes from './routes';
 import stateRouting from './middleware/routing.mw';
+import configurePassport from './config/passport';
+
 
 const CLIENT_PATH = join(__dirname, '../../client');
 
@@ -12,6 +14,9 @@ let app = express();
 app.use(morgan('dev'));
 app.use(express.static(CLIENT_PATH));
 app.use(express.json());
+
+configurePassport(app);
+
 
 app.use('/api', routes);
 
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,10 +1,20 @@
 import { Router } from 'express';
-import peopleRouter from './people';
 import blogRouter from './blog';
+import usersRouter from './users';
+import authRouter from './auth';
+import { isLoggedIn, tokenMiddleware } from '../middleware/auth.mw';
 
 let router = Router();
 
-router.use('/people', peopleRouter);
+router.use('/auth', authRouter);
+
+router.route('*')
+    .post(tokenMiddleware, isLoggedIn)
+    .put(tokenMiddleware, isLoggedIn)
+    .delete(tokenMiddleware, isLoggedIn);
+
 router.use('/blog', blogRouter);
+router.use('/users', usersRouter);
+
 
 export default router;
